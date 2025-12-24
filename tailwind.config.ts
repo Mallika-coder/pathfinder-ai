85af0c29a97d6ac0e85f4d8afad012b92109e8a8
Mallika Verma <vermamallika372@gmail.com>
2025-12-24 23:46:39 +0530

    new

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,30 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
 
-export default {
-  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+// export default {
+//   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         crisis: {
+//           bg: "#0f172a",
+//           panel: "#020617",
+//           danger: "#ef4444",
+//           safe: "#10b981",
+//           action: "#34C759",
+//           accent: "#38bdf8"
+//         }
+//       }
+//     }
+//   },
+//   plugins: []
+// } satisfies Config;
+
+
+module.exports = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -10,11 +33,9 @@ export default {
           panel: "#020617",
           danger: "#ef4444",
           safe: "#10b981",
-          action: "#34C759",
-          accent: "#38bdf8"
-        }
-      }
-    }
+        },
+      },
+    },
   },
-  plugins: []
-} satisfies Config;
+  plugins: [],
+};
